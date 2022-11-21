9df3e1b679dda3fc656c09a713abd01c2025059a
Darien Niamir <darien.niamir@gmail.com>
2022-11-21 01:02:52 -0800

    Create subsystem for MAX31820 temperature sensor

---
+++ lib/Sub_Systems/max31820_ss.cpp
@@ -0,0 +1,20 @@
+# include <MAX31820_SS.h>
+
+MAX31820_SS::MAX31820_SS(OneWire &oneWire) : DallasTemperature(&oneWire) {}
+
+void MAX31820_SS::configure_system() {
+    DallasTemperature::begin();
+}
+
+void MAX31820_SS::read_data(bool print_data) {
+
+    DallasTemperature::requestTemperatures(); // Send the command to get temperatures
+    float tempC = DallasTemperature::getTempCByIndex(0);
+
+    if (!print_data) {return;}
+
+    Serial.print("MAX 31820 Temperature: ");
+    Serial.print(tempC);
+    Serial.print("degC");
+    Serial.println();
+}
---
+++ lib/Sub_Systems/max31820_ss.h
@@ -0,0 +1,13 @@
+# include <DallasTemperature.h>
+# include <arduino_i2c.h>
+
+class MAX31820_SS : public DallasTemperature {
+
+  public:
+    // Constructor
+    MAX31820_SS(OneWire &oneWire);
+
+    // Sensor
+    void read_data(bool print_data);
+    void configure_system();
+};

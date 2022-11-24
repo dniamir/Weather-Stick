14377a34e74fd950ecf0b344769546e663138f1a
Darien Niamir <darien.niamir@gmail.com>
2022-11-24 13:31:52 -0800

    Add LED blink

--- src/main.cpp
+++ src/main.cpp
@@ -7,10 +7,12 @@
 # include <bme680_ss.h>
 
 #define ONE_WIRE_BUS 27
-const int tsl_interrupt_pin = 35;  // the number of the pushbutton pin
+const int tsl_interrupt_pin = 35;
 
 int I2C_SDA = 33;
 int I2C_SCL = 32;
+const int LED_PIN = 26;
+const float led_blink_time = 1000;
 
 // Define sensor systems
 ArduinoI2C arduino_i2c;
@@ -21,6 +23,16 @@ BME680_SS bme_system = BME680_SS(arduino_i2c);
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire = OneWire(ONE_WIRE_BUS);
 MAX31820_SS temp_sensor = MAX31820_SS(oneWire);
+
+// Never ending task
+void toggle_led1(void * parameter){
+  for(;;) { // infinite loop
+    digitalWrite(LED_PIN, HIGH);  // // Turn the LED on
+    vTaskDelay(*((float*)parameter) / 2 / portTICK_PERIOD_MS);
+    digitalWrite(LED_PIN, LOW);  // Turn the LED off
+    vTaskDelay(*((float*)parameter) / 2 / portTICK_PERIOD_MS);
+  }
+}
 
 void setup() {
   Serial.begin(9600);
@@ -41,6 +53,9 @@ void setup() {
   // Pin modes
   light_system.interrupt_pin = tsl_interrupt_pin;
   pinMode(tsl_interrupt_pin, INPUT);
+  pinMode(LED_PIN, OUTPUT);
+
+  xTaskCreate(toggle_led1, "Toggle LED1",  1000, (void*)&led_blink_time, 1, NULL);
 
 }
 

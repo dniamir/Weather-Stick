a9a2f742248198fd36273d6c6fe41c3ded3be3af
Darien Niamir <darien.niamir@gmail.com>
2022-11-24 12:49:47 -0800

    Update main method for BME readings

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 # include <scd4x_ss.h>
 # include <tsl2591_ss.h>
 # include <max31820_ss.h>
-# include <bme680.h>
+# include <bme680_ss.h>
 
 #define ONE_WIRE_BUS 27
 const int tsl_interrupt_pin = 35;  // the number of the pushbutton pin
@@ -16,7 +16,7 @@ int I2C_SCL = 32;
 ArduinoI2C arduino_i2c;
 SCD4x_SS co2_system;
 TSL2591_SS light_system = TSL2591_SS(arduino_i2c);
-BME680 bme_sensor = BME680(arduino_i2c);
+BME680_SS bme_system = BME680_SS(arduino_i2c);
 
 // Setup a oneWire instance to communicate with any OneWire devices (not just Maxim/Dallas temperature ICs)
 OneWire oneWire = OneWire(ONE_WIRE_BUS);
@@ -25,29 +25,18 @@ MAX31820_SS temp_sensor = MAX31820_SS(oneWire);
 void setup() {
   Serial.begin(9600);
 
+  // Setup I2C
   Wire.begin(I2C_SDA, I2C_SCL);
 
   // Soft Reset Systems
+  bme_system.soft_reset();
+  delay(200);
 
   // Setup systems
   light_system.configure_system();
   co2_system.configure_system(Wire);
   temp_sensor.configure_system();
-
-  // Setup BME
-  bme_sensor.soft_reset();
-  delay(200);
-  Serial.println(bme_sensor.read_field(0xD0));
-
-  Serial.println("Start Cal code read");
-  bme_sensor.read_cal_codes();
-  bme_sensor.write_field("osrs_h", 0b101);
-  bme_sensor.write_field("osrs_t", 0b101);
-  bme_sensor.write_field("osrs_p", 0b101);
-  bme_sensor.write_field("filter", 0b010);
-  bme_sensor.write_field("run_gas", 0b1);
-  bme_sensor.write_field("nb_conv", 0b0000);
-  bme_sensor.write_field("mode", 0b01);
+  bme_system.configure_system();
 
   // Pin modes
   light_system.interrupt_pin = tsl_interrupt_pin;
@@ -63,18 +52,7 @@ void loop() {
   co2_system.read_data(true);
   light_system.read_data(true);
   temp_sensor.read_data(true);
-
-    // Read BME Data
-  int32_t bme_temperature = bme_sensor.read_temperature();
-  int32_t bme_pressure = bme_sensor.read_pressure();
-  int32_t bme_humidity = bme_sensor.read_humidity();
-  Serial.print("BME 680 Temperature: ");
-  Serial.println((float)bme_temperature / 100);
-  Serial.print("BME 680 Pressure: ");
-  Serial.println((float)bme_pressure);
-  Serial.print("BME 680 Humidity: ");
-  Serial.println((float)bme_humidity / 1000);
-  Serial.println();
+  bme_system.read_data(true);
 
   // // Interrupts
   Serial.print("Light Sensor Interrupt: ");

fbb9ff0f2ad10ff3d5246a1e0271346fe9e3586c
Darien Niamir <darien.niamir@gmail.com>
2023-03-08 00:57:27 -0800

    Sub-system for sending data over TCP server

---
+++ embedded/lib/Sub_Systems/wifi_ss.cpp
@@ -0,0 +1,49 @@
+# include <wifi_ss.h>
+
+WIFI_SS::WIFI_SS() {}
+
+WIFI_SS::WIFI_SS(const char* ssid, const char* password) {
+    WIFI_SS::connect_to_wifi(ssid, password);
+}
+
+void WIFI_SS::connect_to_wifi(const char* ssid, const char* password) {
+
+    WiFi.begin(ssid, password);
+
+    // Make 5 attempts to connect to wifi
+    uint8_t attempts = 5;
+    uint8_t attempt = 1;
+    while (!WIFI_SS::wifi_status) {
+        delay(500);
+        Serial.print("Attempt ");
+        Serial.print(attempt);
+        Serial.println(" to connect to Wifi...");
+
+        if(attempt == attempts) {
+            break;
+        }
+
+        attempt++;
+        WIFI_SS::wifi_status = (WiFi.status() == WL_CONNECTED);
+    }
+    // Print local IP address and start web server
+    Serial.print("WiFi connected! ");
+    Serial.print("IP address: ");
+    Serial.println(WiFi.localIP());
+}
+
+void WIFI_SS::send_message(int32_t message[]) {
+
+    WiFiClient client;
+    client.connect(WIFI_SS::server_ip_address, WIFI_SS::server_port);
+    Serial.println(WIFI_SS::server_ip_address);
+    Serial.println(WIFI_SS::server_port);
+
+    for (int i=0; i<14; i++) {
+        client.print(message[i]);
+        client.print(", ");
+
+    }
+    client.stop();
+
+}
---
+++ embedded/lib/Sub_Systems/wifi_ss.h
@@ -0,0 +1,17 @@
+# include <WiFi.h>
+
+class WIFI_SS {
+
+  public:
+    // Constructor
+    WIFI_SS();
+    WIFI_SS(const char* ssid, const char* password);
+
+    const char* server_ip_address = "192.168.4.102";
+    const uint16_t server_port = 8090;
+    bool wifi_status = false;
+
+    void connect_to_wifi(const char* ssid, const char* password);
+    void send_message(int32_t message[]);
+
+};
